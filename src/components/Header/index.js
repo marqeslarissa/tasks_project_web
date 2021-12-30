3f60a599ec2a4bcf4bee6f652cd680bb2ab9b12d
Larissa Marques <larissamarques@live.com.pt>
2021-12-30 16:48:33 +0000

    create header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import * as S from "./styles";
 import logo from '../../assets/elipse4.png'
+import notifications from '../../assets/notifications.png'
 
 function Header() {
   return (
     <S.Container>
-     <S.LeftSide>
-       <img src={logo} alt="logo" />
-     </S.LeftSide>
-     <S.RightSide>
-     </S.RightSide>
-     </S.Container>
+      <S.LeftSide>
+        <img src={logo} alt="logo" />
+      </ S.LeftSide>
+      <S.RightSide>
+        <a href="#">Inìcio</a>
+        <span className="dividir"/>
+        <a href="#">Nova Tarefa</a>
+        <span className="dividir"/>
+        <a href="#" id="notifications">
+          <img src={notifications} alt="Notificação"/>
+          <span>5</span>
+        </a>
+      </ S.RightSide>
+    </ S.Container>
   );
 }
 
