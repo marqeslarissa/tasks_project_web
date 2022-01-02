6672a6d82d3ddd8037525091bbef84e39bb10fd8
Larissa Marques <larissamarques@live.com.pt>
2022-01-02 00:45:34 +0000

    Connecting Frontend to Backend

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styles";
+import api from "../../services/api";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import FilterCard from "../../components/FilterCard";
 import TaskCard from "../../components/TaskCard";
 
 function Home() {
-  const [filterActived, setFilterActived] = useState(); // recebe nome do estado e a funcao que atualiza o estado
+  const [filterActived, setFilterActived] = useState('all'); // recebe nome do estado e a funcao que atualiza o estado
+  const [tasks, setTasks] = useState([]);
+
+  async function loadTasks() {
+    await api.get(`/task/filter/${filterActived}/11:11:11:11:11`)
+      .then(response => {
+        setTasks(response.data)
+        console.log(response.data)
+      })
+  }
+  useEffect(() => {  //useEffect é uma function distapada toda vez que a tela for carregada
+    loadTasks(); //chama a funcao e carrega as informacoes do DB
+  }, [filterActived]) //toda vez que mudar o estado do filtro o loadtasks é carregado
+
   return (
     <S.Container>
       <Header />
@@ -31,18 +45,11 @@ function Home() {
         <h3>TAREFAS</h3>
       </S.Title>
       <S.Content>
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
+        {
+          tasks.map(t => (
+            <TaskCard type={t.title} title={t.title} when={t.when}/>
+          ))
+        }
       </S.Content>
       <Footer />
     </S.Container>
